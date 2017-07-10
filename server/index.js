d9276a11ad729e99ac49ebe80c8b573b2096e305
Ada323 <mag242@gmail.com>
2017-07-10 10:42:09 -0500

    Add express router

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
-var config = require('../db/config.js'); // run mongod 
+var router = require('./router')
 
 const app = express();
+
+// transpile and serve all static files using webpack
 
 app.use('/static', express.static(path.join(__dirname, '../client/public')));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+
+// implement express router
+
+app.use('/', router);
 
 
 app.listen(3000, function () {
--- server/router.js
+++ server/router.js
@@ -1,3 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+var config = require('../db/config.js'); // run mongod
 
-// add express router here!!
+var router = express.Router();
+
+// authentication
+
+// comment fetching and creating
+
+module.exports = router;
