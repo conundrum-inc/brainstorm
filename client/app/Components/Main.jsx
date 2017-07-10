1c6fa77a86c40a47101ddfc3c8c69dbc9f6843f2
Jordan Bishop <bishopj1292@gmail.com>
2017-07-10 14:44:01 -0500

    random commit to make sure rebase is updated

--- client/app/Components/Main.jsx
+++ client/app/Components/Main.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 const Main = (props) => {
   return (
     <div>
+      <button onClick={/* drop down menu componenet renders, animates */}>Menu hamburger</button>
+      <button onClick={/* starts new session */}> Create new session </button>
       I am the Main page!
     </div>
   )
