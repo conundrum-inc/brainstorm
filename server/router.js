8be8eb4806a953fd8d19e7877b3494072a0d23ce
Ada323 <mag242@gmail.com>
2017-07-10 11:41:16 -0500

    Add some helper functions for db

--- server/router.js
+++ server/router.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-var config = require('../db/config.js'); // run mongod
+var config = require('../db/config'); // run mongod
+var helpers = require('../db/helpers.js');
 
 var router = express.Router();
 
-// authentication
+// authentication -- signup, login, logout (use passport to implement google auth)
 
-// comment fetching and creating
+
+// create new session
+
+
+
+
+// create new comment
+
+
+// fetch all comments for a given session
+
+
+
+
+// upvote a comment
+
+
+// downvote a comment
+
 
 module.exports = router;
