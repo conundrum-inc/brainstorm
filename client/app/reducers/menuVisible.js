ce66e5f70f3d44053fc55dd804c22928c2a66e09
Jordan Bishop <bishopj1292@gmail.com>
2017-07-11 14:12:22 -0500

    minor updates to menu vis reducers

--- client/app/reducers/menuVisible.js
+++ client/app/reducers/menuVisible.js
@@ -1,10 +1,10 @@
 function viewMenu(state = {isMenuVisible: false}, action) {
   switch (action.type) {
-    case 'SHOW_DETAIL':
-      console.log("Showing Detail");
+    case 'SHOW_MENU':
+      console.log("Showing Menu");
       return {isMenuVisible: true}; //if this doesn't work, try {...state, isMenuVisible: true}
-    case 'HIDE_DETAIL':
-      console.log('Hiding Detail');
+    case 'HIDE_MENU':
+      console.log('Hiding Menu');
       return {isMenuVisible: false}; //if this doesn't work, try {...state, isMenuVisible: false}
     default:
       return state;
