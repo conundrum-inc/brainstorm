5f630e1d5e8ce85daadb15654f1dfc04fb0817a0
Hank <hbowen1123@gmail.com>
2017-07-10 19:55:45 -0500

    Created super basic list creator with d3.

--- client/app/Components/Session.jsx
+++ client/app/Components/Session.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import { select } from 'd3-selection';
+
+
+const ideas = [];
 
 
 const Session = (props) => {
+
+  const draw = (val) => {
+    console.log(val);
+    select("#session").append("div")
+    ideas.push(val);
+    var p = select("#session").selectAll("div")
+              .data(ideas)
+              .text(function(d,i){return i + ": " + d;})
+
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    draw(document.getElementById("myVal").value)
+    document.getElementById("myVal").value = '';
+    console.log('clicked!')
+    return false;
+  }
+
   return (
     <div>
       I am a session!
-      <svg
-        width="100%"
-        height="100%"
-      >
-      </svg>
+      <form name="myform" onSubmit={handleClick}>
+        <input name="Submit"  type="submit" value="Post idea"></input>
+        <input type="text" id="myVal" placeholder="Add some text"></input>
+      </form>
+      <div id="session"></div>
+      
+
     </div>
   )
 }
