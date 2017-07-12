1fd619251f0c0a8ac645d31c2abbe1d8709d20b4
Hank <hbowen1123@gmail.com>
2017-07-12 16:57:16 -0500

    click on svg renders circle with hello world text inside.

--- client/d3/d3helpers.js
+++ client/d3/d3helpers.js
@@ -1,12 +1,35 @@
 import * as d3 from 'd3'
 
+const data = ["hello world"]
+
 const drawCircle = function(x, y, size) {
-      d3.select('svg').append("circle")
-          .attr('class', 'click-circle')
-          .attr("cx", x)
-          .attr("cy", y)
-          .attr("r", size);
-    }
+
+  var elem = d3.select("svg").selectAll("g myCircleText")
+                .data(data)
+
+  var elemEnter = elem.enter()
+    .append("g")
+    .attr("transform", function(d){ return "translate("+x+","+y+")"})
+
+  var circle = elemEnter.append("circle")
+    .attr("r", size)
+    .attr("stroke","black")
+    .attr("fill","white")
+
+  elemEnter.append("text")
+    .attr("dx", function(d){return -20})
+    .text(function(d){return data[0]})
+
+  elemEnter.on('click', function() {
+    console.log('clicked!')
+  })
+
+  // d3.select('svg').append("circle")
+  //     .attr("cx", x)
+  //     .attr("cy", y)
+  //     .attr("r", size)
+  //     .style("color","pink");
+}
 
 export const click = function() {
       var coords = d3.mouse(d3.select('svg').node());
