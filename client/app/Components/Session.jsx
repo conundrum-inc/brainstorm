0237d24aa43723cbfcdb0fbc2b9f2111ff53cb71
Hank <hbowen1123@gmail.com>
2017-07-11 19:03:55 -0500

    Moved d3 functions into their own folder

--- client/app/Components/Session.jsx
+++ client/app/Components/Session.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import * as d3 from 'd3'
 import { withFauxDOM } from 'react-faux-dom'
+import { click } from '../../d3/d3helpers.js'
 
 class Session extends React.Component {
 
   componentDidMount() {
 
-    const drawCircle = function(x, y, size) {
-      console.log('Drawing circle at', x, y, size);
-      d3.select('svg').append("circle")
-          .attr('class', 'click-circle')
-          .attr("cx", x)
-          .attr("cy", y)
-          .attr("r", size);
-    }
-
-    const click = function() {
-      var coords = d3.mouse(d3.select('svg').node());
-      console.log(coords);
-      drawCircle(coords[0], coords[1], 50);
-    }
+    
 
     const board = this.props.connectFauxDOM('div','board')
     d3.select(board)
@@ -27,12 +15,9 @@ class Session extends React.Component {
       .attr("height", "100%")
       .attr("width", "100%")
       .on('click', function() {
-
-        const x = d3.event.layerX
-        const y = d3.event.layerY
         click()
       });
-    this.props.animateFauxDOM(800);
+    
   }
 
   render() {
