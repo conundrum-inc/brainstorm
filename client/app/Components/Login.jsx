02537b041eb87f76c2057d19a1427a80c8f6ff0e
Jordan Bishop <bishopj1292@gmail.com>
2017-07-13 12:26:50 -0500

    working on login component

--- client/app/Components/Login.jsx
+++ client/app/Components/Login.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const Login = (props) => {
   return (
     <div>
-      I am the Login page!
+      <h2>Welcome to Brainstorm!</h2>
+      <div>Sign in with Google to get started.</div>
+      <button className="btn google-oauth"> Sign In</button>
     </div>
   )
 }
