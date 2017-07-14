42ce4afe7299e57330dfebe531729358c62c61bb
Jordan Bishop <bishopj1292@gmail.com>
2017-07-14 16:13:33 -0500

    Move add comment button  to modal part 2

--- client/app/Components/Main.jsx
+++ client/app/Components/Main.jsx
@@ -8,7 +8,6 @@ const Main = (props) => {
   return (
     <div>
       <h2>I am the Main component!</h2>
-      {/* <button className="add-comment" onClick={() => props.addComment('123', '345', '678', 'first comment', 'yassssss')}>Add Comment</button> */}
       <Session showDetail={props.showDetail}
                hideDetail={props.hideDetail}
                detailViewVisible={props.detailViewVisible}
