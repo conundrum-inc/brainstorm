1276aaad570a8335154677da22287709c975c529
Jordan Bishop <bishopj1292@gmail.com>
2017-07-15 15:15:23 -0500

    Refactor menu component

--- client/app/Components/Menu.jsx
+++ client/app/Components/Menu.jsx
@@ -1,40 +1,26 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import { PROFILE_PAGE_ROUTE,
          LOGOUT_PAGE_ROUTE,
          SESSIONS_PAGE_ROUTE,
          MAIN_PAGE_ROUTE
        } from '../routes.js'
 
-const Menu = (props) => {
-  if (props.menuVisible) {
-    return (
-      <div onClick={() => props.toggleClick()}>
-        {[
-          {route: MAIN_PAGE_ROUTE, label: 'Home' },
-          {route: PROFILE_PAGE_ROUTE, label: 'My Profile'},
-          {route: LOGOUT_PAGE_ROUTE, label: 'Logout'}
-        ].map((link) => {
-          return (
-            <div key={link.label}>
-              <NavLink
-                to={link.route}
-                exact
-                >
-                  {link.label}
-                </NavLink>
-              </div>
-            )
-          })
-        }
-      </div>
-    )
-  } else {
-    return (
-      <button className="menu-burger" onClick={() => props.toggleClick()}>Menu Burger</button>
-    )
-  }
+const Menu = () => {
+  return (
+    <DropdownButton id="dropdown-btn-menu" bsStyle="info" title="Menu">
+      <MenuItem key="1" href={MAIN_PAGE_ROUTE}>
+        <NavLink to={PROFILE_PAGE_ROUTE}>Home</NavLink>
+      </MenuItem>
+      <MenuItem key="2" href={PROFILE_PAGE_ROUTE}>
+        <NavLink to={PROFILE_PAGE_ROUTE}>My Profile</NavLink>
+      </MenuItem>
+      <MenuItem key="3" href={LOGOUT_PAGE_ROUTE}>
+        <NavLink to={LOGOUT_PAGE_ROUTE}>Logout</NavLink>
+      </MenuItem>
+    </DropdownButton>
+  )
 }
 
 export default Menu
