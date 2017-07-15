e4816757fdb396dde63641e86df327d250661131
Jordan Bishop <bishopj1292@gmail.com>
2017-07-15 11:25:37 -0500

    implement new comment form input on Node

--- client/app/Components/NodeDetail.jsx
+++ client/app/Components/NodeDetail.jsx
@@ -5,12 +5,22 @@ const NodeDetail = (props) => {
     <div>
       <h2>Idea Title</h2>
       <p>This idea is literally the best idea ever. We should totally do this idea. You know what's awesome? This idea. Why? Because! Vote for Pedro!</p>
-      <button className="add-comment" onClick={() => this.props.addComment('123', '345', '678', 'first comment', 'yassssss')}>Add Comment</button>
-      <button>upvote</button>
-      <button>downvote</button>
+      <button className="upvote" onClick="blahblahblah"></button>
+      <button className="downvote" onClick="blahblahblah"></button>
       <h4>Node Child 1</h4>
       <h4>Node Child 2</h4>
       <h4>Node Child 3</h4>
+      <form>
+        <label>
+          Title:
+          <input type="text" name="title" />
+        </label>
+        <label>
+          Description:
+          <input type="text" name="text" />
+        </label>
+        <input type="submit" value="Add Comment" onClick={() => this.props.addComment('Jordan', '345', '678', 'first comment', 'yassssss')} />
+      </form>
     </div>
   )
 }
