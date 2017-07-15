b7fe0b8b46706a89e20c9afc62ca374c25843e87
Misha Hoo <hoomisha@gmail.com>
2017-07-15 17:26:15 -0500

    add user and comments to persistent state

--- client/app/store.js
+++ client/app/store.js
@@ -2,6 +2,7 @@ import { createStore, compose, applyMiddleware } from 'redux';
 // import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import thunk from 'redux-thunk';
+import { loadState, saveState } from './localStorage'
 
 // import the root reducer
 import rootReducer from './reducers/index';
@@ -25,11 +26,20 @@ import rootReducer from './reducers/index';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
-  // defaultState,
+  persistedState,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+store.subscribe(() => {
+  saveState({
+    user: store.getState().user,
+    comments: store.getState().comments
+  });
+});
 
 // export const history = syncHistoryWithStore(browserHistory, store);
 
