9f41ed773b717b0e72ab24394c74f30d90df4e13
Jordan Bishop <bishopj1292@gmail.com>
2017-07-20 16:10:17 -0500

    implement starting css styles for modals

--- client/app/Components/Menu.jsx
+++ client/app/Components/Menu.jsx
@@ -10,34 +10,36 @@ import { PROFILE_PAGE_ROUTE,
        } from '../routes.js'
 
 const Menu = (props) => {
-  // <a href={PROFILE_PAGE_ROUTE}><img src={props.user.image}/></a>
-
   return (
     <div>
       <ReactModal isOpen={props.menuVisible}
                   contentLabel="Menu Modal"
                   shouldCloseOnOverlayClick={props.menuVisible}
+                  className="ReactModal__Content--after-open--menu"
       >
-        <Button onClick={props.hideMenu}>X</Button>
-        <a href={PROFILE_PAGE_ROUTE}><img src={props.user.image} /></a>
-        <a href={PROFILE_PAGE_ROUTE}><h3>{props.user.name}</h3></a>
-        <h2>My Sessions</h2>
-        <p>Session 1</p>
-        <p>Session 2</p>
-        <p>Session 3</p>
-        <Button bsStyle="info"
-          className="add-comment"
-          onClick={() => { props.showCreateSession(); props.hideMenu(); }}>
-          New Session!
-        </Button>
-        <Button bsStyle="info" href={LOGOUT_PAGE_ROUTE}>
-          <NavLink to={LOGOUT_PAGE_ROUTE}></NavLink>
-          Logout
-        </Button>
+        <Button className="menu-exit-btn" onClick={props.hideMenu}>X</Button>
+        <div className="menu-modal-content">
+          <a href={PROFILE_PAGE_ROUTE}><img src={props.user.image} /></a>
+          <a href={PROFILE_PAGE_ROUTE}><h3>{props.user.name}</h3></a>
+          <h2>My Sessions</h2>
+          <p>Session 1</p>
+          <p>Session 2</p>
+          <p>Session 3: a really long session name to test the auto width css property </p>
+          <Button bsStyle="info"
+            className="add-comment"
+            onClick={() => { props.showCreateSession(); props.hideMenu(); }}>
+            New Session!
+          </Button>
+          <Button bsStyle="info" href={LOGOUT_PAGE_ROUTE}>
+            <NavLink to={LOGOUT_PAGE_ROUTE}></NavLink>
+            Logout
+          </Button>
+        </div>
       </ReactModal>
 
       <ReactModal isOpen={props.createSessionVisible}
         contentLabel="Detail Modal"
+        className=".ReactModal__Content--after-open--new-session"
         shouldCloseOnOverlayClick={props.createSessionVisible}
         >
           <Button onClick={props.hideCreateSession}>X</Button>
