1e5b4a5a209ca8f681292cfdd3081e046401ae4a
Jordan Bishop <bishopj1292@gmail.com>
2017-07-22 16:02:51 -0500

    Delete unnecessary comments in Menu

--- client/app/Components/Menu.jsx
+++ client/app/Components/Menu.jsx
@@ -85,28 +85,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(null, mapDispatchToProps)(Menu)
-
-/* <ButtonGroup>
-  <Button bsStyle="info" href={PROFILE_PAGE_ROUTE}>
-    <NavLink to={PROFILE_PAGE_ROUTE}></NavLink>
-    My Profile
-  </Button>
-  <Button bsStyle="info" className="add-comment" onClick={() => props.showCreateSession()}>New Session!</Button>
-  <Button bsStyle="info" href={LOGOUT_PAGE_ROUTE}>
-    <NavLink to={LOGOUT_PAGE_ROUTE}></NavLink>
-    Logout
-  </Button>
-</ButtonGroup>
-<ReactModal isOpen={props.createSessionVisible}
-  contentLabel="Detail Modal"
-  shouldCloseOnOverlayClick={props.createSessionVisible}
-  >
-    <Button onClick={props.hideCreateSession}>X</Button>
-    <CreateSessionDetail
-      addComment={props.addComment}
-      currentNode={props.currentNode}
-      setNode={props.setNode}
-      updateNode={props.updateNode}
-      thunkCreateSession={props.thunkCreateSession}
-    />
-  </ReactModal> */
