7b2f10cdc50361c0e23277b5af71d2a69ebc1d32
Misha Hoo <hoomisha@gmail.com>
2017-07-22 11:15:11 -0500

    Reposition svg graph to make though nodes more visible

--- client/app/Components/Graph.jsx
+++ client/app/Components/Graph.jsx
@@ -136,7 +136,7 @@ class Graph extends React.Component {
   render() {
     return (
         <svg  className="session-container" width="100%" height="100%">
-          <g ref='graph' width="100%" height="100%" />
+          <g ref='graph' transform="translate(0,100)" width="100%" height="100%" />
         </svg>
       );
   }
