fae9086e02372932bf70912a860a1bb9377f9ed3
Ada323 <mag242@gmail.com>
2017-07-28 15:02:16 -0500

    Console logs to debug socket

--- server/index.js
+++ server/index.js
@@ -136,7 +136,7 @@ server.listen(3000, function () {
 })
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected', 'socket.rooms', socket.rooms, 'socket id', id);
   socket.on('join session', function(session_id) {
     console.log('joining session: ', session_id)
     socket.join(session_id, () => {
